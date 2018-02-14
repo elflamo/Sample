b06bc1d989014a3c752e2083f6e31d0688722146
Shikhar <shikhar@bigdrop.io>
2018-02-14 20:01:18 +0530

    complete

--- Rest/urls.py
+++ Rest/urls.py
@@ -10,6 +10,8 @@ urlpatterns = [
     url(r'^token/verify', verify_jwt_token),
     url(r'^token/refresh', refresh_jwt_token),
     url(r'^signup$', views.SignupView.as_view(), name="signup"),
-    url(r'^forgot/password$', views.ForgotPasswordView.as_view(), name="initiate-reset-password"),
+    url(r'^send/reset/otp$', views.SendResetOtpView.as_view(), name="send-otp-email"),
+    url(r'^check/otp$', views.CheckOtpView.as_view(), name="check-otp"),
+    url(r'^reset/password$', views.ResetPasswordView.as_view(), name="reset-password")
 
 ]
--- main/views.py
+++ main/views.py
@@ -9,6 +9,7 @@ from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 
+
 class SignupView(CreateAPIView):
 
     serializer_class = serializers.SignupSerializer
@@ -18,10 +19,10 @@ class SignupView(CreateAPIView):
     def create(self, request, *args, **kwargs):
 
         if request.data.get('password') != request.data.get('confirm_password'):
-            return Response(data={'error':'Password mismatch'}, status=status.HTTP_200_OK, content_type="application/json")
+            return Response(data={'error': 'Password mismatch'}, status=status.HTTP_200_OK, content_type="application/json")
 
         if models.User.objects.filter(email=request.data.get('email')).first() is not None:
-            return Response(data={'error':'User with this email already exists'}, status=status.HTTP_200_OK, content_type="application/json")
+            return Response(data={'error': 'User with this email already exists'}, status=status.HTTP_200_OK, content_type="application/json")
 
         user_obj = models.User.objects.create(
             email=request.data.get('email'),
@@ -30,20 +31,57 @@ class SignupView(CreateAPIView):
         user_obj.set_password(request.data.get('password'))
         user_obj.save()
 
-        return Response(data={'success':'Created'}, status=status.HTTP_201_CREATED, content_type="application/json")
+        return Response(data={'success': 'Created'}, status=status.HTTP_201_CREATED, content_type="application/json")
 
 
-class ForgotPasswordView(GenericAPIView):
+class SendResetOtpView(GenericAPIView):
 
     authentication_classes = ()
     permission_classes = ()
-    serializer_class = serializers.ForgotPasswordSerializer
 
     def post(self, request, *args, **kwargs):
 
         user_obj = models.User.objects.filter(email=request.data['email']).first()
 
         if user_obj is None:
-            return Response(data={'error':'No user found'}, status=status.HTTP_203_NON_AUTHORITATIVE_INFORMATION, content_type="application/json")
+            return Response(data={'error': 'No user found'}, status=status.HTTP_203_NON_AUTHORITATIVE_INFORMATION, content_type="application/json")
+        else:
+            otp = models.Otp.generate(user_obj)
+            return Response(data={'success': 'Otp sent on email','user': user_obj.email}, status=status.HTTP_200_OK, content_type="application/json")
 
-        pass
+
+class CheckOtpView(GenericAPIView):
+
+    authentication_classes = ()
+    permission_classes = ()
+
+    def post(self, request, *args, **kwargs):
+
+        otp = request.data['otp']
+        user_obj = models.User.objects.filter(email=request.data['email']).first()
+
+        if models.Otp.objects.filter(user=user_obj).last().otp != otp:
+            return Response(data={'error':'Wrong Otp'}, status=status.HTTP_203_NON_AUTHORITATIVE_INFORMATION, content_type="application/json")
+
+        else:
+            return Response(data={'success':'Allow reset','user': user_obj.email}, status=status.HTTP_200_OK, content_type="application/json")
+
+
+class ResetPasswordView(GenericAPIView):
+
+    authentication_classes = ()
+    permission_classes = ()
+
+    def post(self, request, *args, **kwargs):
+
+        password = request.data['password']
+        confirm_password = request.data['confirm_password']
+
+        if password != confirm_password:
+            return Response(data={'error': 'Password mismatch'}, status=status.HTTP_203_NON_AUTHORITATIVE_INFORMATION, content_type="application/json")
+
+        else:
+            user_obj = models.User.objects.filter(email=request.data['email']).first()
+            user_obj.set_password(confirm_password)
+            user_obj.save()
+            return Response(data={'success':'Password has been reset'}, status=status.HTTP_200_OK, content_type="application/json")
